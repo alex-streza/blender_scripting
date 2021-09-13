b83ddcd0a38f56565e721f63b31579cdcf3f83c1
alex-streza <alex.streza@okapistudio.com>
2021-09-14 00:37:12 +0300

    changes

--- nfts/memphrane/config.py
+++ nfts/memphrane/config.py
@@ -8,24 +8,18 @@ blend_file_path = base_dir + '/GenerateMaterial.blend'
 description = "Humanoidz rule."
 base_image_uri = "https://memphrane/nft/"
 edition_dna_prefix = 0
-edition_start = 1
-edition_size = 1
+edition_starts_at = 1
+edition_ends_at = 10
+edition_size = 10
 
-# adds a rarity to the configuration. This is expected to correspond with a directory containing the rarity for each defined variation
-# @param _id - id of the rarity
-# @param _from - number in the edition to start this rarity from
-# @param _to - number in the edition to generate this rarity to
-# @return a rarity object used to dynamically generate the NFTs
 def add_rarity (_id, _from, _to):
   rarity_weight = {
     "value": _id,
     "from": _from,
     "to": _to,
-    "variation_percent": {}
   }
   return rarity_weight
 
-# get the name without last 4 characters -> slice .png from the name
 def clean_name (_str):
   name = _str[:-4];
   return name
@@ -37,20 +31,71 @@ def get_file_elements(_path, _rarity):
   for file_name in entries:
     elements.append({
       "name": clean_name(file_name),
-      "path": path + '/' + file_name
+      "value": path + '/' + file_name
     })
   return elements
 
 def get_light_elements(_path, _rarity):
   light_variations = {
-    "common": ["#ffffff", "#EABFCB", "#D9D9D9"],
-    "rare": ["#3C6E71", "#284B63"],
-    "super_rare": ["#DFEFCA", "#AFC2D5", "#CCDDD3"],
-    "legendary": ["#7D2E68", "#251351 ", "#DE3C4B", "#2BC016"],
+    "common": [
+      {
+        "id": "0/0",
+        'value': "#ffffff",
+        'weight': 50,
+      },
+      {
+        "id": "1/0",
+        'value': "##EABFCB",
+        'weight': 15,
+      },
+      {
+        "id": "2/0",
+        'value': "#D9D9D9",
+        'weight': 35,
+      }
+    ],
+    "rare": [
+      {
+        "id": "0/0",
+        'value': "#3C6E71",
+        'weight': 50,
+      },
+      {
+        "id": "1/0",
+        'value': "#284B63",
+        'weight': 50,
+      }
+    ],
+    "super_rare": [{
+      "id": "0/0",
+      'value': "##AFC2D5",
+      'weight': 100,
+    }],
+    "legendary": [
+      {
+        "id": "0/0",
+        'value': "#7D2E68",
+        'weight': 25,
+      },
+      {
+        "id": "1/0",
+        'value': "#251351",
+        'weight': 25,
+      },
+      {
+        "id": "2/0",
+        'value': "#DE3C4B",
+        'weight': 25,
+      },
+      {
+        "id": "3/0",
+        'value': "#2BC016",
+        'weight': 25,
+      }
+    ],
   }
   return light_variations[_rarity]
 
-# reads the filenames of a given folder and returns it with its name and path
 def get_elements (_path, _rarity, _variation_type):
   switcher = {
     "image": get_file_elements,
@@ -61,56 +106,28 @@ def get_elements (_path, _rarity, _variation_type):
 
   return switcher.get(_variation_type)(_path, _rarity)
 
-# generate variation used to dynamically generate the NFTs
-# it will contain the path to
-# @param _variation_id - name of the variation
 def add_variation (_variation_id, _variation_type):
   if not _variation_id:
     print('error adding variation, parameters id required')
 
   elements = {}
-  element_ids_for_rarity = {}
 
   for i in range(len(rarity_weights)):
     rarity = rarity_weights[i]['value']
     elements_for_rarity = get_elements(current_dir + _variation_id, rarity, _variation_type)
 
-    element_ids_for_rarity[rarity] = []
     for j in range(len(elements_for_rarity)):
       id = str(edition_dna_prefix) + "/"+ str(j)
-      elements_for_rarity[j].update({'id': id})
-      element_ids_for_rarity[rarity].append(id)
+      if _variation_type != 'light':
+        elements_for_rarity[j].update({'id': id, 'weight': 100})
 
     elements[rarity] = elements_for_rarity
 
   elements_for_variation = {
     'id': _variation_id,
     'elements': elements,
-    'element_ids_for_rarity': element_ids_for_rarity
   }
   return elements_for_variation
-
-# adds variation-specific percentages to use one vs another rarity
-# @param _rarityId - the id of the rarity to specifiy
-# @param _variation_id - the id of the variation to specifiy
-# @param _percentages - an object defining the rarities and the percentage with which a given rarity for this variation should be used
-def add_rarity_percent_for_variation (_rarityId, _variation_id, _percentages):
-  rarity_found = False
-
-  for rarity_weight in rarity_weights:
-    if rarity_weight["value"] == _rarityId:
-      percent_array = []
-      for percent_type in _percentages:
-        percent_array.append({
-          "id": percent_type,
-          "percent": _percentages[percent_type]
-        })
-      rarity_weight['variation_percent'][_variation_id] = percent_array;
-      rarity_found = True
-
-  if rarity_found == False:
-    print('rarity '+ _rarityId + ' not found, failed to add percentage information')
-
 
 ##############
 # BEGIN CONFIG
@@ -119,25 +136,13 @@ rarity_weights = [
   add_rarity('legendary', 1, 1),
   add_rarity('super_rare', 2, 3),
   add_rarity('rare', 4, 6),
-  add_rarity('common', 7, 10)
+  add_rarity('common', 7, edition_ends_at)
 ]
 
-# create required variations
-# for each variation, call 'add_variation' with the id
-# the id would be the name of the folder in your input directory, e.g. 'background' for ./input/background
 variations = [
   add_variation('skin', 'image'),
   add_variation('eye', 'image'),
   add_variation('background', 'hdri'),
   add_variation('body', 'model'),
-  # add_variation('light', 'light'),
+  add_variation('light', 'light'),
 ]
-
-# provide any specific percentages that are required for a given variation and rarity level
-# all provided options are used based on their percentage values to decide which variation to select from
-add_rarity_percent_for_variation('super_rare', 'skin', { 'legendary': 10, 'super_rare': 20, 'rare': 30, 'common': 40 })
-
-# add_rarity_percent_for_variation('super_rare', 'eye', { 'legendary': 25, 'super_rare': 25, 'rare': 25, 'common': 25 })
-# add_rarity_percent_for_variation('common', 'background', { 'legendary': 10, 'super_rare': 20, 'rare': 30, 'common': 40 })
-# add_rarity_percent_for_variation('common', 'body', { 'legendary': 25, 'super_rare': 25, 'rare': 25, 'common': 25 })
-# add_rarity_percent_for_variation('common', 'light', { 'legendary': 25, 'super_rare': 25, 'rare': 25, 'common': 25  })
--- nfts/memphrane/generate.py
+++ nfts/memphrane/generate.py
@@ -1,17 +1,20 @@
 import sys
-sys.path += ["C:/Users/astre/Blockchain/generative_art/nfts/memphrane/"]
+sys.path += ['C:/Users/astre/Blockchain/generative_art/nfts/memphrane/']
 
 import config
 import bpy
-from random import random, choice
+from random import random
 import time
 import json
 import math
 import shutil
+from PIL import Image, ImageFont, ImageDraw
 
 render_path = config.render_path
 blend_file_path = config.blend_file_path
 edition_size = config.edition_size
+edition_starts_at = config.edition_starts_at
+edition_ends_at = config.edition_ends_at
 base_dir = config.base_dir
 edition = str(config.edition_dna_prefix)
 variations = config.variations
@@ -27,42 +30,27 @@ def select_object_by_name(_name = 'Model'):
 
 def is_dna_unique(_dna_list = [], _dna = []):
 	found_dna = any(''.join(elem) == ''.join(_dna) for elem in _dna_list)
-	return (len(found_dna) == 0)
+	return found_dna == 0
 
-def get_random_rarity(_rarity_options):
-	random_percent = random() * 100
-	percent_count = 0
+def get_rarity(_nft_index):
+  rarity = 'Unknown'
+  for rarity_weight in rarity_weights:
+    if _nft_index >= rarity_weight['from'] and _nft_index <= rarity_weight['to']:
+      rarity = rarity_weight['value']
 
-	for i in range(len(_rarity_options)):
-		percent_count += _rarity_options[i].percent
-		if percent_count >= random_percent:
-			print('use random rarity' +  _rarity_options[i].id)
-			return _rarity_options[i].id
-	return _rarity_options[0].id
-
-def get_rarity(_edition_count):
-  rarity_for_edition = []
-  if len(rarity_for_edition) == 0:
-    for rarity_weight in rarity_weights:
-      for i in range(rarity_weight['from'], rarity_weight['to']):
-        rarity_for_edition.append(rarity_weight['value'])
-
-  return rarity_for_edition[edition_size - _edition_count]
+  return rarity
 
 def create_dna(_variations, _rarity):
   rand_nums = []
-  rarity_weight = any(elem['value'] == _rarity for elem in rarity_weights)[0]
 
   for variation in _variations:
-    print(variation)
-    num = math.floor(random() * len(variation['element_ids_for_rarity'][_rarity]))
+    random_num = math.floor(random() * 100) + 1
+    num = '0'
+    for element in variation['elements'][_rarity]:
+      if random_num >= 100 - element['weight']:
+        num = str(variation['id'])
 
-    if rarity_weight and rarity_weight['variation_percent'][variation.id]:
-      rarity_for_layer = get_random_rarity(rarity_weight['variation_percent'][variation.id])
-      num = math.floor(random() * len(variation['element_ids_for_rarity'][rarity_for_layer]))
-      rand_nums.append(variation['element_ids_for_rarity'][rarity_for_layer][num])
-    else:
-      rand_nums.append(variation['element_ids_for_rarity'][_rarity][num])
+    rand_nums.append(num)
 
   return rand_nums
 
@@ -77,22 +65,14 @@ def save_metadata(_nfts):
 def generate_metadata(_dna, _edition, _attributes ):
   date_time = str(time.time())
   temp_metadata = {
-    "dna": _dna,
-    "name": "#" + _edition,
-    "description": "This is an NFT made by the coolest generative code.",
-    "image": base_image_uri + _edition,
-    "date": date_time,
-    "attributes": _attributes
+    'dna': _dna,
+    'name': '#' + _edition,
+    'description': 'This is an NFT made by the coolest generative code.',
+    'image': base_image_uri + _edition,
+    'date': date_time,
+    'attributes': _attributes
   }
   return temp_metadata
-
-def construct_layer_to_dna(_new_dna, _variations, _rarity):
-  print("- construct layer to dna")
-  results = {}
-  for variation in _variations:
-    print(variation)
-
-  return results
 
 # load and set hdri from path
 def load_hdri(_path):
@@ -118,15 +98,15 @@ def load_hdri(_path):
 
   # Link all nodes
   links = node_tree.links
-  link = links.new(node_environment.outputs["Color"], node_background.inputs["Color"])
-  link = links.new(node_background.outputs["Background"], node_output.inputs["Surface"])
+  link = links.new(node_environment.outputs['Color'], node_background.inputs['Color'])
+  link = links.new(node_background.outputs['Background'], node_output.inputs['Surface'])
 
 def hex_to_rgb(colorstring):
-    """ convert #RRGGBB to an (R, G, B) tuple """
+    ''' convert #RRGGBB to an (R, G, B) tuple '''
     colorstring = colorstring.strip()
     if colorstring[0] == '#': colorstring = colorstring[1:]
     if len(colorstring) != 6:
-        raise ValueError("input #%s is not in #RRGGBB format" % colorstring)
+        raise ValueError('input #%s is not in #RRGGBB format' % colorstring)
     r, g, b = colorstring[:2], colorstring[2:4], colorstring[4:]
     r, g, b = [int(n, 16) / 255.0 for n in (r, g, b)]
     return (r, g, b)
@@ -134,7 +114,6 @@ def hex_to_rgb(colorstring):
 def update_light_color(_color = '#ffffff'):
   select_object_by_name('Light')
   for obj in bpy.context.selected_objects:
-    print(hex_to_rgb(_color))
     obj.data.color = hex_to_rgb(_color)
 
 def load_material(_path, _name, _object_name = 'Model'):
@@ -148,66 +127,70 @@ def load_material(_path, _name, _object_name = 'Model'):
     for obj in bpy.context.selected_objects:
       obj.active_material = active_material
 
+def sign_nft(_path, _nft_no):
+  nft_image = Image.open(_path)
+  font = ImageFont.truetype('./Geometria.ttf', 500)
+  image_editable = ImageDraw.Draw(nft_image)
+  image_editable.text((15,15), _nft_no, (237, 230, 211), font=font)
+  nft_image.save(_path)
+
+def generate_nft(_new_dna, _variations, _nft_index):
+  # propagate information about  required layer contained within config into a mapping object
+
+  # load a random body
+
+  # # load a random material
+  # load_material('C:/Users/astre/OneDrive/1_year_of_blender/september 2021/Memprhrane/assets/skin/legendary/galaxy/galaxy.blend', 'Galaxy')
+
+  # # set random light color
+  # update_light_color('#251351')
+
+  # # load random background
+  # load_hdri('C:/Users/astre/OneDrive/1_year_of_blender/september 2021/Memprhrane/assets/background/legendary/outer_space_1/outer_space_2_8k.exr')
+
+  nft_no = str(_nft_index + 1)
+
+  nft_meta = generate_metadata(_new_dna, edition, {})
+
+  save_metadata_single_file(nft_meta, nft_no)
+
+  bpy.context.scene.render.filepath = render_path + nft_no
+  bpy.ops.render.render(write_still=True)
+
+  # shutil.copyfile(blend_file_path, base_dir + '/' + nft_no +'.blend')
+
+  return nft_meta
 
 def generate_nfts():
   select_object_by_name('Model')
-  # holds which dna has already been used during generation
   dna_list_by_rarity = {}
-  # holds metadata for all NFTs
   nfts_meta = []
 
-  # prepare dna_list object
   for rarity_weight in rarity_weights:
     dna_list_by_rarity[rarity_weight['value']] = []
 
   for nft_index in range(edition_size):
-    dna = 0
-    attributes = []
+    nft_no = str(nft_index + 1)
+    print('-----------------')
+    print('creating NFT ' +  nft_no + ' of ' + str(edition_size))
 
-    print("-----------------")
-    print("creating NFT %d of %d", nft_index, edition)
+    rarity = get_rarity(nft_index + 1)
+    print('- rarity: ' + rarity)
 
-		# get rarity from to config to create NFT as
-    rarity = get_rarity(nft_index)
-    print("- rarity: " + rarity)
-
-    # calculate the NFT dna by getting a random part for each layer/feature
-		# based on the ones available for the given rarity to use during generation
     new_dna = create_dna(variations, rarity)
-    while not is_dna_unique(dna_list_by_rarity[rarity], new_dna):
-			# recalculate dna as this has been used before.
-      print("found duplicate DNA " + "-".join(new_dna) + ", recalculate...")
+    print('- dna: ' + str(new_dna))
+
+    sign_nft(render_path + nft_no + ".png", nft_no)
+
+    if is_dna_unique(dna_list_by_rarity[rarity], new_dna):
       new_dna = create_dna(variations, rarity)
 
-    print("- dna: " + "-".join(new_dna))
+      print('- dna: ' + '-'.join(new_dna))
 
-		# propagate information about  required layer contained within config into a mapping object
-    results = construct_layer_to_dna(new_dna, variations, rarity)
+      nfts_meta.append(generate_nft(new_dna, variations, nft_index))
 
-    # load a random body
-
-    # # load a random material
-    # load_material('C:/Users/astre/OneDrive/1_year_of_blender/september 2021/Memprhrane/assets/skin/legendary/galaxy/galaxy.blend', 'Galaxy')
-
-    # # set random light color
-    # update_light_color("#251351")
-
-    # # load random background
-    # load_hdri('C:/Users/astre/OneDrive/1_year_of_blender/september 2021/Memprhrane/assets/background/legendary/outer_space_1/outer_space_2_8k.exr')
-
-    # nft_no = str(nft_index + 1)
-    # nft_meta = generate_metadata(dna, edition, attributes)
-    # nfts_meta.append(nft_meta)
-
-    # save_metadata_single_file(nft_meta, nft_no)
-
-    # bpy.context.scene.render.filepath = render_path + active_material.name
-    # bpy.ops.render.render(write_still=True)
-
-    # generate blend file copy
-    # shutil.copyfile(blend_file_path, base_dir + '/' + nft_no +'.blend')
-
+    else: print('DNA exists!')
 
   save_metadata(nfts_meta)
 
-# generate_nfts()
+generate_nfts()
